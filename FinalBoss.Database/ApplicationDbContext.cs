a557ecba625af9979645c38e2d9852fcb1496841
Adam Stirtan <adam.stirtan@fernsoftware.com>
2021-02-02 21:34:42 -0500

    EF project

---
+++ FinalBoss.Database/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+using FinalBoss.ObjectModel;
+
+namespace FinalBoss.Database
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Game> Games { get; set; }
+    }
+}
