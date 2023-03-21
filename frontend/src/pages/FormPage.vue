8c64b3d71c6874baa495e1d7ac536b1f4afd24d6
Adam Stirtan <adamstirtan@gmail.com>
2023-03-20 23:15:30 -0400

    Moved directories

---
+++ frontend/src/pages/FormPage.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+let id = 0
+
+const newTodo = ref('')
+const hideCompleted = ref(false)
+const todos = ref([
+    { id: id++, text: 'Learn HTML', done: true },
+    { id: id++, text: 'Learn JavaScript', done: true },
+    { id: id++, text: 'Learn Vue', done: false }
+])
+
+function addTodo() {
+    todos.value.push({ id: id++, text: newTodo.value, done: false })
+    newTodo.value = ''
+}
+
+function removeTodo(todo) {
+    todos.value = todos.value.filter((t) => t !== todo)
+}
+</script>
+
+<template>
+    <form @submit.prevent="addTodo">
+        <input v-model="newTodo">
+        <button>Add Todo</button>
+    </form>
+    <ul>
+        <li v-for="todo in todos" :key="todo.id">
+            <input type="checkbox" v-model="todo.done">
+            <span :class="{ done: todo.done }">{{ todo.text }}</span>
+            <button @click="removeTodo(todo)">X</button>
+        </li>
+    </ul>
+    <button @click="hideCompleted = !hideCompleted">
+        {{ hideCompleted ? 'Show all' : 'Hide completed' }}
+    </button>
+</template>
+
+<style>
+.done {
+    text-decoration: line-through;
+}
+</style>
