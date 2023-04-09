e92cdb420c1f939f49f8a0039f45d6f4417a3a5f
Adam Stirtan <adamstirtan@gmail.com>
2023-04-08 21:28:05 -0400

    Merges DTO to entity

---
+++ backend/FinalBoss.ObjectModel/Attributes/NotMergableAttribute.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace FinalBoss.ObjectModel.Attributes
+{
+    [AttributeUsage(AttributeTargets.Property)]
+    public class NotMergableAttribute : Attribute
+	{ }
+}
+
--- backend/FinalBoss.ObjectModel/BaseEntity.cs
+++ backend/FinalBoss.ObjectModel/BaseEntity.cs
@@ -1,13 +1,40 @@
 ﻿using System;
+using System.Linq;
+
+using FinalBoss.ObjectModel.Attributes;
 
 namespace FinalBoss.ObjectModel
 {
     public abstract class BaseEntity
     {
+        [NotMergable]
         public long Id { get; set; }
 
+        [NotMergable]
         public DateTimeOffset Created { get; set; }
 
+        [NotMergable]
         public DateTimeOffset LastModified { get; set; }
+
+        public void Merge<T>(T target, T source)
+        {
+            Type type = typeof(T);
+
+            var properties = type
+                .GetProperties()
+                .Where(x =>
+                    !Attribute.IsDefined(x, typeof(NotMergableAttribute)) &&
+                    x.CanRead && x.CanWrite);
+
+            foreach (var property in properties)
+            {
+                var value = property.GetValue(source, null);
+
+                if (value != null)
+                {
+                    property.SetValue(target, value, null);
+                }
+            }
+        }
     }
 }
