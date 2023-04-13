eb35a21c2787ca64fba66ab7014e714b4a8f7b8d
Adam Stirtan <adamstirtan@gmail.com>
2023-04-12 23:50:48 -0400

    Apply migrations at startup

--- backend/GameWave.Api/Program.cs
+++ backend/GameWave.Api/Program.cs
@@ -1,4 +1,6 @@
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
 using Serilog;
@@ -9,7 +11,16 @@ namespace GameWave.Api
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var context = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+
+                context.Database.Migrate();
+            }
+
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
